522713ce0f4fca470b92e9667691e12f2065a9d2
Arunimavpa <arunimaajithvp14@gmail.com>
2025-04-19 03:08:11 +0530

    Update wsgi.py

--- boutique/wsgi.py
+++ boutique/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
-import os
 
+import os
 from django.core.wsgi import get_wsgi_application
 
+# this was still pointing at the example
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_application.settings')
+# point at your real settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boutique.settings')
 
 application = get_wsgi_application()
