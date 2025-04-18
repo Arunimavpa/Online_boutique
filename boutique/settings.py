ef52694f62e6485f4e47c177644167b4a927d9c1
Arunimavpa <arunimaajithvp14@gmail.com>
2025-04-19 03:22:30 +0530

    Update setings.py

--- boutique/settings.py
+++ boutique/settings.py
@@ -1,5 +1,3 @@
-# settings.py (excerpt)
-
 import os
 from pathlib import Path
 import dj_database_url
@@ -7,20 +5,28 @@ from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
-DEBUG      = os.getenv('DEBUG', 'False').lower() in ('true', '1')
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+SECRET_KEY   = os.getenv('SECRET_KEY', get_random_secret_key())
+DEBUG        = os.getenv('DEBUG', 'False').lower() in ('true','1')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'online-boutique.onrender.com').split(',')
+
+# >>> MISSING LINES <<<
+ROOT_URLCONF    = 'boutique.urls'
+WSGI_APPLICATION = 'boutique.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL'),
         conn_max_age=600,
-        ssl_require=True
+        ssl_require=True,
     )
 }
 
 INSTALLED_APPS = [
-    # …
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'online',
 ]
@@ -28,47 +34,33 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    # …
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [ BASE_DIR / 'templates' ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# settings.py (excerpt)
 
-import os
-from pathlib import Path
-import dj_database_url
-from django.core.management.utils import get_random_secret_key
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
-DEBUG      = os.getenv('DEBUG', 'False').lower() in ('true', '1')
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
-
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
-
-INSTALLED_APPS = [
-    # …
-    'django.contrib.staticfiles',
-    'online',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    # …
-]
-
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# … any other settings (EMAIL, MEDIA, etc.) …
